bdc47dfab609786f61333aa610696fe914bed0cd
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-06-07 08:56:29 +0700

    praktek is function

---
+++ 000 - Praktek/IsFunction.php
@@ -0,0 +1,12 @@
+<?php
+
+// lengkapnya di https://www.php.net/manual/en/ref.var.php
+
+$data = 100;
+
+var_dump(is_string($data));
+var_dump(is_bool($data));
+var_dump(is_int($data));
+var_dump(is_float($data));
+var_dump(is_array($data));
+var_dump(is_null($data));
