2178cf2bc873034130223acefe36367b06962369
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-06-10 11:30:31 +0700

    view menghapus todolist

--- todolist-dasar/View/ViewRemoveTodoList.php
+++ todolist-dasar/View/ViewRemoveTodoList.php
@@ -1,10 +1,27 @@
 <?php
 
+require_once "../Helper/Input.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+
 /*
     View untuk menampilkan form hapus todo
 */
 
 function viewRemoveTodoList()
 {
-    //
+    echo "MENGAPUS  TODO" . PHP_EOL;
+
+    $pilihan = (int) input("Nomor (x untuk batal)");
+
+    if ($pilihan == "x") {
+        echo "Batal menghapus todo" . PHP_EOL;
+    } else {
+        $success = removeTodoList($pilihan);
+
+        if ($success) {
+            echo "Sukses menghapus todo nomor $pilihan" . PHP_EOL;
+        } else {
+            echo "Gagal menghapus todo nomor $pilihan" . PHP_EOL;
+        }
+    }
 }
