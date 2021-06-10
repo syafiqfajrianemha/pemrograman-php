3e35bfc2b9210bc3f2cfbdca515299c03fcdcf97
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-06-10 10:38:41 +0700

    view menampilkan todolist

--- todolist-dasar/View/ViewShowTodoList.php
+++ todolist-dasar/View/ViewShowTodoList.php
@@ -1,10 +1,36 @@
 <?php
 
+require_once "../BusinessLogic/ShowTodoList.php";
+require_once "../BusinessLogic/AddTodoList.php";
+require_once "../BusinessLogic/RemoveTodoList.php";
+require_once "../Helper/Input.php";
+
 /*
     View untuk menampilkan semua daftar todolist
 */
 
 function viewShowTodoList()
 {
-    //
+    while (true) {
+        showTodoList();
+
+        echo "MENU" . PHP_EOL;
+        echo "1. Tambah Todo" . PHP_EOL;
+        echo "2. Hapus Todo" . PHP_EOL;
+        echo "x. Keluar" . PHP_EOL;
+
+        $pilihan = input("Pilih");
+
+        if ($pilihan == "1") {
+            viewAddTodoList();
+        } else if ($pilihan == "2") {
+            viewRemoveTodoList();
+        } else if ($pilihan == "x") {
+            break;
+        } else {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+        }
+    }
+
+    echo "Sampai Jumpa Lagi" . PHP_EOL;
 }
