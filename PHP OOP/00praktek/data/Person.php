26de213e55e58b0d40089d1c7eeb28424a085291
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-06-14 18:34:10 +0700

    praktek : function atau method

--- PHP OOP/00praktek/data/Person.php
+++ PHP OOP/00praktek/data/Person.php
@@ -6,4 +6,10 @@ class Person
     var string $name;
     var ?string $address = null;
     var string $country = "Indonesia";
+
+    // function atau method
+    function sayHeloo(string $name)
+    {
+        echo "Hello $name" . PHP_EOL;
+    }
 }
