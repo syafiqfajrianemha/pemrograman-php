bb2eb6d1b4e813fae5923ae3cff86a484bba1890
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-06-24 05:07:48 +0700

    praktek : constructor overriding

--- PHP OOP/00praktek/data/Manager.php
+++ PHP OOP/00praktek/data/Manager.php
@@ -3,6 +3,13 @@
 class Manager
 {
     var string $name;
+    var string $title;
+
+    public function __construct(string $name = "", string $title = "Manager")
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
 
     function sayHello(string $name): void
     {
@@ -12,6 +19,12 @@ class Manager
 
 class VicePresident extends Manager
 {
+    public function __construct(string $name = "")
+    {
+        // tidak wajib, tapi direkomendasikan
+        parent::__construct($name, "VP");
+    }
+
     function sayHello(string $name): void
     {
         echo "Hi $name, my name is VP {$this->name}" . PHP_EOL;
