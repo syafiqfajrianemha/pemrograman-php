3ae1a789c6538c30cb97db580028df90c8ca7d67
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-06-30 05:39:31 +0700

    praktek : trait inheritance

--- PHP OOP/00praktek/data/SayGoodBye.php
+++ PHP OOP/00praktek/data/SayGoodBye.php
@@ -49,13 +49,18 @@ class ParentPerson
     }
 }
 
-class Person extends ParentPerson
+trait All
 {
     use SayGoodBye, SayHello, HasName, CanRun {
         // bisa di override
         // hello as private;
         // goodBye as private;
     }
+}
+
+class Person extends ParentPerson
+{
+    use All;
 
     public function run(): void
     {
