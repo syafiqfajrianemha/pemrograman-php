bd2bb06f17c81ac6944ffee94d71ee2c575e751e
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-07-06 05:27:01 +0700

    praktek : generator

---
+++ PHP OOP/00praktek/Generator.php
@@ -0,0 +1,32 @@
+<?php
+
+// ribet
+function getGenap(int $max): Iterator
+{
+    $array = [];
+    for ($i = 1; $i <= $max; $i++) {
+        if ($i % 2 == 0) {
+            $array[] = $i;
+        }
+    }
+    return new ArrayIterator($array);
+}
+
+foreach (getGenap(100) as $value) {
+    echo "Genap : {$value}" . PHP_EOL;
+}
+
+
+// simple => generator / menggunakan yield keyword
+function getGanjil(int $max): Iterator
+{
+    for ($i = 1; $i <= $max; $i++) {
+        if ($i % 2 == 1) {
+            yield $i;
+        }
+    }
+}
+
+foreach (getGanjil(100) as $value) {
+    echo "Ganjil : {$value}" . PHP_EOL;
+}
