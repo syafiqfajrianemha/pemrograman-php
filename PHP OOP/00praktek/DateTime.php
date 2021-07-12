d6ba0d9152fe58a774d3c1505045ec3774787cc1
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-07-13 06:08:53 +0700

    praktek : datetime

---
+++ PHP OOP/00praktek/DateTime.php
@@ -0,0 +1,28 @@
+<?php
+
+$dateTime = new DateTime();
+$dateTime->setDate(2003, 7, 11);
+$dateTime->setTime(10, 10, 10, 0);
+
+$dateTime->add(new DateInterval("P1Y"));
+
+$minusOneMonth = new DateInterval("P1M");
+$minusOneMonth->invert = true;
+$dateTime->add($minusOneMonth);
+
+var_dump($dateTime);
+
+$now = new DateTime();
+var_dump($now);
+$now->setTimezone(new DateTimeZone("America/Toronto"));
+var_dump($now);
+
+$string = $now->format("Y-m-d H:i:s");
+echo "Waktu Saat Ini : {$string}" . PHP_EOL;
+
+$date = DateTime::createFromFormat("Y-m-d H:i:s", "2021-10-10 10:10:10", new DateTimeZone("Asia/Jakarta"));
+if ($date) {
+    var_dump($date);
+} else {
+    echo "Format Salah" . PHP_EOL;
+}
