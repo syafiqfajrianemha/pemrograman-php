a8566c8f54b96eb8474812eaab89db1cfbd3330c
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-07-14 06:06:40 +0700

    praktek : reflection

---
+++ PHP OOP/00praktek/Reflection.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "exception/ValidationException.php";
+require_once "data/LoginRequest.php";
+require_once "helper/ValidationUtil.php";
+
+$request = new LoginRequest();
+$request->username = "syafiq";
+$request->password = "rahasia";
+
+// ValidationUtil::validate($request);
+
+ValidationUtil::validateReflection($request);
+
+class RegisterUserRequest
+{
+    public ?string $name;
+    public ?string $address;
+    public ?string $email;
+}
+
+$register = new RegisterUserRequest();
+$register->name = "Syafiq";
+$register->address = "Jateng";
+$register->email = "syafiq@gmail.com";
+
+ValidationUtil::validateReflection($register);
