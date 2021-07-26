830dfe88b2f18cc10269fc8c82b2d7dc1e1be9c7
Syafiq Fajrian Emha <syafiqfajrianemha11@gmail.com>
2021-07-27 05:58:19 +0700

    praktek : validation untuk function overriding

---
+++ belajar-php8/00praktek/FunctionOverridingClass.php
@@ -0,0 +1,17 @@
+<?php
+
+class ParentClass
+{
+    public function method(string $name)
+    {
+        //
+    }
+}
+
+class ChildClass extends ParentClass
+{
+    public function method(int $name)
+    {
+        //
+    }
+}
---
+++ belajar-php8/00praktek/FunctionOverridingTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+trait SampleTrait
+{
+    public abstract function sampleFunction(string $name): string;
+}
+
+class SampleClass
+{
+    use SampleTrait;
+
+    public function sampleFunction(string $name): string
+    {
+        //
+    }
+}
